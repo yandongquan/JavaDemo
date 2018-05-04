a1efdea5f163d6a3dd0622513e129531f2090eff
yandongquan <yandongquanlove@gmail.com>
2018-05-04 17:21:43 +0800

     :alien: type of date

---
+++ src/com/javakz/typeofdata/TypeOfData.java
@@ -0,0 +1,13 @@
+package com.javakz.typeofdata;
+
+/**
+ * @Author: yandq
+ * @Description: 数据类型
+ * @Date: Create in 17:16 2018/5/4
+ * @Modified By:
+ */
+public class TypeOfData {
+    public static void main(String[] args) {
+
+    }
+}
