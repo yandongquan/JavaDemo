55b02bd9e23679d522cd47fc987ec0b87a65a4db
yandongquan <yandongquanlove@gmail.com>
2018-05-06 23:59:53 +0800

    hello world

--- src/com/javakz/typeofdata/TypeOfData.java
+++ src/com/javakz/typeofdata/TypeOfData.java
@@ -8,6 +8,16 @@ package com.javakz.typeofdata;
  */
 public class TypeOfData {
     public static void main(String[] args) {
+        byte state = '1';
+//        short
 
+        
+        int age = 25;
+        long earthPopulation = 200000000000000L;
+        double salary = 14120.6D;
+        float e = 2.718281828f;
+        double d = 88888882.7182818288888888d;
+        String name = "张三";
+        System.out.println(d);
     }
 }
