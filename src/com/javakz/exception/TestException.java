3bd9312baeb01a75a0b6febbbb331589a5dd7d1a
yandongquan <yandongquanlove@gmail.com>
2018-05-09 18:57:11 +0800

    :neckbeard: add exception

---
+++ src/com/javakz/exception/TestException.java
@@ -0,0 +1,14 @@
+package com.javakz.exception;
+
+/**
+ * @Author: yandq
+ * @Description:
+ * @Date: Create in 18:53 2018/5/9
+ * @Modified By:
+ */
+public class TestException {
+    public static void main(String[] args) {
+        char a = 'a';
+//        int i = 10/0;
+    }
+}
