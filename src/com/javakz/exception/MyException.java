bb590080dcb8a64c3a0f6936bd85061e7ea69c54
yandongquan <yandongquanlove@gmail.com>
2018-05-09 18:59:50 +0800

    :neckbeard: add exception

---
+++ src/com/javakz/exception/MyException.java
@@ -0,0 +1,16 @@
+package com.javakz.exception;
+
+/**
+ * @Author: yandq
+ * @Description:
+ * @Date: Create in 18:57 2018/5/9
+ * @Modified By:
+ */
+public class MyException extends Exception {
+    public MyException() {
+    }
+
+    public MyException(String message) {
+        super(message);
+    }
+}
