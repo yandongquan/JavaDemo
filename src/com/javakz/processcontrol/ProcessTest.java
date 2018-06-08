4b5a9303ad0cc77dd6b5da5a55148be6bda2d3dc
yandongquan <yandongquanlove@gmail.com>
2018-06-08 16:13:36 +0800

    :neckbeard: update process controll

--- src/com/javakz/processcontrol/ProcessTest.java
+++ src/com/javakz/processcontrol/ProcessTest.java
@@ -15,15 +15,14 @@ public class ProcessTest {
         }*/
 
         // if-else 双分支选择结构
-        int i = (int)(10*Math.random());
+        /*int i = (int)(10*Math.random());
         if (i>5) {
             System.out.println("i 大于5");
         } else {
             System.out.println("i 小于5");
-        }
+        }*/
 
-        //
-        int  age = (int)(100*Math.random());
+        int age = (int) (100 * Math.random());
         System.out.print("年龄是" + age + ", 属于");
         if (age < 15) {
             System.out.println("儿童, 喜欢玩！");
@@ -40,8 +39,8 @@ public class ProcessTest {
         }
 
         char c = 'a';
-        int rand = (int)(26*Math.random());
-        char c2 = (char)(c+rand);
+        int rand = (int) (26 * Math.random());
+        char c2 = (char) (c + rand);
         System.out.print(c2 + ":");
         switch (c2) {
             case 'a':
@@ -49,12 +48,77 @@ public class ProcessTest {
             case 'i':
             case 'o':
             case 'u':
-                System.out.println(" 元音 "); break;
+                System.out.println(" 元音 ");
+                break;
             case 'y':
             case 'w':
-                System.out.println(" 半元音 "); break;
+                System.out.println(" 半元音 ");
+                break;
             default:
                 System.out.println(" 辅音 ");
+        }
+
+        int i = 0;
+        int sum = 0;
+        while (i <= 100) {
+            sum += i;
+            i++;
+        }
+        System.out.println("Sum= " + sum);
+
+        int a = 0;
+        while (a < 0) {
+            System.out.println(a);
+            a++;
+        }
+        System.out.println("-----");
+        a = 0;
+        do {
+            System.out.println(a);
+            a++;
+        } while (a < 0);
+
+        int total = 0;
+        System.out.println("Begin");
+        while (true) {
+            total++;
+            int z = (int) Math.round(100 * Math.random());
+            if (z == 88) {
+                break;
+            }
+        }
+        System.out.println("Game over, used " + total + " times.");
+
+        for (int k = 100; k < 150; k++) {
+            if (k % 3 == 0) {
+                continue;
+            }
+            System.out.println(k);
+        }
+
+
+        int count = 0;
+        outer:
+        for (int k = 101; k < 150; k++) {
+            for (int j = 2; j < k / 2; j++) {
+                if (k % j == 0) {
+                    continue outer;
+                }
+            }
+            System.out.println(k + " ");
+        }
+
+        System.out.println("---------------------99乘法表---------------------------");
+        for (int k=1; k<=9; k++) {
+            for (int j=1; j<=k; j++) {
+                if (k*j<10) {
+                    System.out.print(j+"*"+k+"= "+k*j+ "    ");
+                } else {
+                    System.out.print(j+"*"+k+"="+k*j+ "    ");
+                }
+
+            }
+            System.out.println("");
         }
     }
 }
